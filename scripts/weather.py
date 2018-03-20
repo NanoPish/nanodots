b07f2b75ef8d456e9f250d9e097741a6e0b88f90
paul <paul@belloc.ovh>
2018-03-20 15:29:50 +0000

    font fix

--- scripts/weather.py
+++ scripts/weather.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import pyowm
 
 owm = pyowm.OWM('2505ce29adcf3f3d6caf15b18e617417')  # You MUST provide a valid API key
@@ -6,7 +7,14 @@ owm = pyowm.OWM('2505ce29adcf3f3d6caf15b18e617417')  # You MUST provide a valid 
 # owm = pyowm.OWM(API_key='your-API-key', subscription_type='pro')
 
 # Search for current weather in London (Great Britain)
-observation = owm.weather_at_place('London,GB')
+observation = owm.weather_at_place('Paris,FR')
 w = observation.get_weather()
-print(w)                      # <Weather - reference time=2013-12-18 09:20,
-# status=Clouds>
+
+# Weather details
+#print (w.get_wind())                  # {'speed': 4.6, 'deg': 330}
+#print (w.get_humidity())              # 87
+#print (w.get_temperature('celsius').)  # {'temp_max': 10.5, 'temp': 9.7, 'temp_min': 9.0}
+
+current_temp = w.get_temperature('celsius')
+output = current_temp["temp"]
+print (str(output))
